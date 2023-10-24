c96afcca0d492cd0c53425d1fff76d4e88186838
AlbertLnz <albert2000_lanza@hotmail.com>
2023-10-24 13:25:44 +0200

    changing config of deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo 'Running composer'
-composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+# composer global require hirak/prestissimo
+composer install
  
 echo 'Caching config...'
 php artisan config:cache
@@ -10,4 +10,4 @@ echo 'Caching routes...'
 php artisan route:cache
  
 echo 'Running migrations...'
-php artisan migrate --force
+php artisan migrate && php artisan migrate:fresh --seed

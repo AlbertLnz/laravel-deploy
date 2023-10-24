6b038f8553547a55f64a00fa05bb7dacaf3ccb10
AlbertLnz <albert2000_lanza@hotmail.com>
2023-10-24 16:51:48 +0200

    new changesssss

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
- echo "Running composer"
- composer global require hirak/prestissimo
- composer install --no-dev --working-dir=/var/www/html
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
 
- echo "Caching config..."
- php artisan config:cache
+echo "generating application key..."
+php artisan key:generate --show
 
- echo "Caching routes..."
- php artisan route:cache
+echo "Caching config..."
+php artisan config:cache
 
- echo "Running migrations..."
- php artisan migrate --force
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
